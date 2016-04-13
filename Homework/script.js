f52e6785c5e957d574327acf1d65dfbacaf63af0
JaapN <jaap.nieuwenhuizen@student.uva.nl>
2016-04-13 15:07:46 +0200

    Test

---
+++ Homework/script.js
@@ -0,0 +1,13 @@
+console.log('test');
+alert('test');
+
+var dataset = document.getElementById('rawdata');
+dataset.split(',');
+for (var datum in dataset)
+{
+    new Date(datum);
+}
+for (var temperatuur in dataset)
+{
+    //;
+}

e62f8d80a78059197b64908ccf25551260b99793
JaapN <jaap.nieuwenhuizen@student.uva.nl>
2016-05-20 20:52:01 +0200

    D3LinkedViews
    Final?

--- Homework/Week5/D3LinkedViews/loadingdata_LinkedViews.py
+++ Homework/Week5/D3LinkedViews/loadingdata_LinkedViews.py
@@ -6,7 +6,7 @@ output = []
 # load data into arrays
 import csv
 with open('HPIcountries.csv', 'r') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')
+    reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
     	output.append({"HPIrank": row[0], "country": row[1], "subregion": row[2], "lifeExpectancy": row[3], "WellBeing": row[4],
          "HappyLifeYears": row[5], "Footprint_gha_capita": row[6], "HPI": row[7], "Population": row[8], "GDP_capita": row[9],"governanceRank": row[10]})
